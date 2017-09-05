30ed31bf989f02a1b2501ede942cdb1976bdb66d
pcjedi <georgwendorf@gmail.com>
2017-09-05 09:34:49 -0400

    sec as json

--- mbloc/settings.py
+++ mbloc/settings.py
@@ -7,12 +7,19 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-from setting_secret import SECRET_KEY, postgrespw
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+import json
+with open('settings_sec.json') as jf:
+    pws=json.load(jf)
+
+SECRET_KEY = pws['SECRET_KEY']
+postgrespw = pws['postgrespw']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
